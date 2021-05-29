5884ab4747e4414501ea2c34e1ba6bac3e6ab005
mukul98s <mymukul112@gmail.com>
2021-05-29 16:11:45 +0530

    Basic Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
