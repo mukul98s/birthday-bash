026dea010218c2960d0dde1536623f229e9d32d1
Mukul Sharma <mymukul112@gmail.com>
2021-06-02 20:07:51 +0530

    Changed Username Minimum Length (#4)

--- BirthdayBash-Backend/helper/validation.js
+++ BirthdayBash-Backend/helper/validation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const userSignUpSchema = Joi.object()
   .keys({
     email: Joi.string().email().min(5).max(50).required(),
-    username: Joi.string().min(5).max(50).required(),
+    username: Joi.string().min(2).max(50).required(),
     password: Joi.string().min(8).required(),
     dob: Joi.date().required(),
     bio: Joi.string().max(100).optional(),
