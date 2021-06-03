4743c268ec35c1b94e920015414fe22f9b3aafd5
mukul98s <mymukul112@gmail.com>
2021-06-03 10:30:37 +0530

    Header Component

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <header>
+        <div className="container">
+          <h1>Birthday Bash</h1>
+        </div>
+      </header>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  header {
+    background-color: var(--dark-header);
+
+    h1 {
+      text-align: center;
+      padding: 20px 10px;
+      font-weight: 700;
+      text-transform: uppercase;
+      font-size: 2.25rem;
+      letter-spacing: var(--letter-spacing-advance);
+      text-shadow: var(--text-shadow-dark);
+      color: var(--color-primary);
+      font-family: "Shadows Into Light", cursive;
+    }
+  }
+`;
+
+export default Header;
