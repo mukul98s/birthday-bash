ee5989397ce2a8ac5daccb43eaddc3ec7adc2cc1
mukul98s <mymukul112@gmail.com>
2021-06-03 10:30:06 +0530

    Header and Social Component

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import cake from "../assets/cake.svg";
-import instagram from "../assets/instagram.svg";
-import facebook from "../assets/facebook.svg";
-import google from "../assets/google.svg";
-import twitter from "../assets/twitter.svg";
 import axios from "axios";
+import { Header, Social } from "../Components";
+import { Link } from "react-router-dom";
+
+import cake from "../assets/cake.svg";
 
 const Home = () => {
   axios
@@ -17,58 +16,30 @@ const Home = () => {
 
   return (
     <Wrapper>
-      <header>
-        <div className="container">
-          <h1>Birthday Bash</h1>
-        </div>
-      </header>
+      <Header />
       <section className="container">
         <div className="banner">
           <img src={cake} alt="cake" />
         </div>
         <div>
-          <button className="button">Login</button>
-          <button className="button">signup</button>
+          <Link className="button link-button" to="/login">
+            login
+          </Link>
+          <Link className="button link-button" to="/signup">
+            Signup
+          </Link>
         </div>
 
         <article>
           <h4>Or sign up</h4>
         </article>
-
-        <div className="button-group">
-          <div>
-            <img src={instagram} alt="" />
-          </div>
-          <div>
-            <img src={google} alt="" />
-          </div>
-          <div>
-            <img src={twitter} alt="" />
-          </div>
-          <div>
-            <img src={facebook} alt="" />
-          </div>
-        </div>
+        <Social />
       </section>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  header {
-    border-bottom: 2px solid white;
-
-    h1 {
-      text-align: center;
-      padding: 20px 10px;
-      font-weight: 700;
-      text-transform: uppercase;
-      font-size: 2.25rem;
-      letter-spacing: var(--letter-spacing-basic);
-      text-shadow: var(--text-shadow-dark);
-    }
-  }
-
   .banner {
     margin: 5rem 15%;
     width: 70%;
@@ -93,20 +64,6 @@ const Wrapper = styled.div`
       font-weight: 700;
       font-size: 1.2rem;
       text-transform: uppercase;
-    }
-  }
-
-  .button-group {
-    padding: 0 1rem;
-    display: flex;
-    justify-content: space-between;
-
-    div {
-      width: 3rem;
-
-      img {
-        width: 100%;
-      }
     }
   }
 `;
