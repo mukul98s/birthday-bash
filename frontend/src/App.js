e0d723d8b8473c5131dcde839ced246191410852
mukul98s <mymukul112@gmail.com>
2021-06-05 10:27:43 +0530

    Renderer login Private Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home, Login, SignUp } from "./Pages/";
+import { Home, Login, SignUp, UserHome } from "./Pages/";
+import { LoginProvider } from "./State/LoginState";
+import PrivateRouteLogin from "./Pages/PrivateRouteLogin";
 
 function App() {
   return (
@@ -10,11 +12,16 @@ function App() {
           <Home />
         </Route>
         <Route path="/login">
-          <Login />
+          <LoginProvider>
+            <Login />
+          </LoginProvider>
         </Route>
         <Route path="/signup">
           <SignUp />
         </Route>
+        <PrivateRouteLogin>
+          <UserHome />
+        </PrivateRouteLogin>
       </Switch>
     </Router>
   );
