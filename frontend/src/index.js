a783e808abed06457b46176995978bf1f488226b
mukul98s <mymukul112@gmail.com>
2021-06-07 20:26:56 +0530

    added Global State

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { GlobalProvider } from "./State/GlobalState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
