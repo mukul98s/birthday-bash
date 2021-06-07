09fd2bb0d0d8c1c32f28089ef58de67ea6aaeef6
mukul98s <mymukul112@gmail.com>
2021-06-07 20:31:51 +0530

    Private Route is working

--- frontend/src/Pages/PrivateRouteLogin.js
+++ frontend/src/Pages/PrivateRouteLogin.js
@@ -1,22 +1,17 @@
 import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
-import { LoginContext } from "../State/LoginState";
+import { Redirect, Route } from "react-router";
+import { GlobalContext } from "../State/GlobalState";
 
 const PrivateRouteLogin = ({ children, ...rest }) => {
-  const { isAuthenthicated, userAccessKey } = useContext(LoginContext);
-
-  const isLogin = isAuthenthicated && userAccessKey !== null;
+  const { isLogin } = useContext(GlobalContext);
   return (
     <Route
       {...rest}
       render={() => {
-        return userAccessKey !== null ? (
-          { children }
-        ) : (
-          <Redirect to="/login"></Redirect>
-        );
+        return isLogin ? children : <Redirect to="/login"></Redirect>;
       }}
-    ></Route>
+    />
   );
 };
+
 export default PrivateRouteLogin;
