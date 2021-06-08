014cfa66d9f1a65c6010d71c39e8d5318bfeb1d3
mukul98s <mymukul112@gmail.com>
2021-06-08 18:39:34 +0530

    🧹 removed unused hooks

--- frontend/src/State/LoginState.js
+++ frontend/src/State/LoginState.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { GlobalContext } from "./GlobalState";
 
 export const LoginAuth = createContext();
