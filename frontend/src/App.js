924b72be6ab9ec6613cb9b07e8dbfe8fe8168fd9
mukul98s <mymukul112@gmail.com>
2021-06-28 18:41:26 +0530

    ➕lazy and profile private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,40 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home, Login, SignUp, UserHome, UserProfile } from "./Pages/";
+import { Home, Login, SignUp } from "./Pages/";
 import { ProvideAuth } from "./State/LoginState";
 import PrivateRouteLogin from "./Pages/PrivateRouteLogin";
 import { SignUpProvider } from "./State/SignupState";
+import { lazy, Suspense } from "react";
+import Loader from "./Components/Loader";
 
 function App() {
+  const UserHome = lazy(() => import("./Pages/UserHome"));
+  const UserProfile = lazy(() => import("./Pages/UserProfile"));
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact={true}>
-          <Home />
-        </Route>
-        <Route path="/login">
-          <ProvideAuth>
-            <Login />
-          </ProvideAuth>
-        </Route>
-        <Route path="/signup">
-          <SignUpProvider>
-            <SignUp />
-          </SignUpProvider>
-        </Route>
-        <PrivateRouteLogin path="/home">
-          <UserHome />
-        </PrivateRouteLogin>
-        <Route path="/profile">
-          <UserProfile />
-        </Route>
-      </Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <ProvideAuth>
+              <Login />
+            </ProvideAuth>
+          </Route>
+          <Route path="/signup">
+            <SignUpProvider>
+              <SignUp />
+            </SignUpProvider>
+          </Route>
+          <PrivateRouteLogin path="/home">
+            <UserHome />
+          </PrivateRouteLogin>
+          <PrivateRouteLogin path="/profile">
+            <UserProfile />
+          </PrivateRouteLogin>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
