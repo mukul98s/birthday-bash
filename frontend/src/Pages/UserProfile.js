be0b7e733e492aec24f01407d12b5aa198af41db
mukul98s <mymukul112@gmail.com>
2021-06-28 18:43:52 +0530

    Bottom Nav and Typo Styles

--- frontend/src/Pages/UserProfile.js
+++ frontend/src/Pages/UserProfile.js
@@ -1,42 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Header } from "../Components";
+import { Header, BottomNavbar } from "../Components";
 
 const UserProfile = () => {
   return (
-    <Wrapper>
+    <React.Fragment>
       <Header />
-      <div className="container">
-        <main>
-          <article>
-            <aside>
-              <img
-                src="https://wallpapercave.com/wp/mJRp4Xn.jpg"
-                alt="profileImage"
-              />
-            </aside>
-            <aside>
-              <h3>Steve Jobs</h3>
-              <h4>Followers : 151M </h4>
-              <h4>Following : 1 </h4>
-            </aside>
-          </article>
-          <div>
-            <p>
-              Founder & CEO of Apple <br />
-              Founder & ex-CEO of NeXT
-              <br />
-              ex-CEO of Pixar
-            </p>
-          </div>
-        </main>
-
-        <div>
-          <h1>Recently Wished</h1>
+      <Wrapper>
+        <div className="container">
+          <main>
+            <article>
+              <aside>
+                <img
+                  src="https://wallpapercave.com/wp/mJRp4Xn.jpg"
+                  alt="profileImage"
+                />
+              </aside>
+              <aside>
+                <h3>Steve Jobs</h3>
+                <h4>Followers : 151M </h4>
+                <h4>Following : 1 </h4>
+              </aside>
+            </article>
+            <div>
+              <p>
+                Founder & CEO of Apple <br />
+                Founder & ex-CEO of NeXT
+                <br />
+                ex-CEO of Pixar
+              </p>
+            </div>
+          </main>
         </div>
-      </div>
-    </Wrapper>
+        <div className="underline"></div>
+        <div className="container">
+          <div>
+            <h1>Recently Wished</h1>
+          </div>
+        </div>
+      </Wrapper>
+      <BottomNavbar />
+    </React.Fragment>
   );
 };
 
@@ -45,19 +50,16 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin: 3rem auto;
+    margin: 2rem auto 1rem;
     padding: 1rem;
     gap: 1.5rem;
-
     article {
       width: 100%;
       display: flex;
       align-items: center;
       justify-content: space-between;
-
       aside:first-child {
         flex: 1;
-
         img {
           width: 100%;
           border-radius: 50%;
@@ -66,15 +68,14 @@ const Wrapper = styled.div`
       aside:last-child {
         flex: 2;
         text-align: center;
-
         h3 {
           text-transform: uppercase;
-          font-size: 2rem;
+          font-size: 1.5rem;
           font-weight: 500;
           letter-spacing: 0.1em;
           margin-bottom: 0.5rem;
+          text-shadow: var(--text-shadow-dark);
         }
-
         h4 {
           font-weight: normal;
           letter-spacing: var(--letter-spacing-basic);
@@ -82,18 +83,17 @@ const Wrapper = styled.div`
         }
       }
     }
-
     div p {
       letter-spacing: var(--letter-spacing-basic);
     }
   }
-
   div h1 {
     text-transform: uppercase;
     text-align: center;
     font-weight: normal;
     letter-spacing: var(--letter-spacing-basic);
-    font-size: 1.6rem;
+    font-size: 1rem;
+    margin: 1rem auto;
   }
 `;
 
