d03b51081dca1f7e694cd9f318ed83c64c347cbc
mukul98s <mymukul112@gmail.com>
2021-06-28 18:44:11 +0530

    LocalStorage is used to store login status

--- frontend/src/State/GlobalState.js
+++ frontend/src/State/GlobalState.js
@@ -3,7 +3,9 @@ import React, { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("isLogin"));
+  localStorage.setItem("isLogin", isLogin);
+
   return (
     <GlobalContext.Provider value={{ isLogin, setIsLogin }}>
       {children}
