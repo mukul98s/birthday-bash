88794f0d332636de39c3e33cd5dc5c06b97a1f21
mukul98s <mymukul112@gmail.com>
2021-08-12 23:27:24 +0530

    feat: removed useless imports

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import axios from "axios";
 import { Header, Social } from "../Components";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
