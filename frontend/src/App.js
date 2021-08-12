a7ebc2cbb75ae791725630d6d51bf42e61b74978
mukul98s <mymukul112@gmail.com>
2021-08-12 23:23:54 +0530

    feat: removed animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Home } from "./Pages/";
 import { ProvideAuth } from "./State/LoginState";
 import PrivateRouteLogin from "./Pages/PrivateRouteLogin";
 import { SignUpProvider } from "./State/SignupState";
 import { lazy, Suspense } from "react";
 import Loader from "./Components/Loader";
-
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   // Lazy Imports
@@ -17,36 +15,32 @@ function App() {
   const SignUp = lazy(() => import("./Pages/SignUp"));
   const Error = lazy(() => import("./Pages/Error"));
 
-  const location = useLocation();
-
   return (
     <Suspense fallback={<Loader />}>
-      <AnimatePresence exitBeforeEnter>
-        <Switch location={location} key={location.key}>
-          <Route path="/" exact={true}>
-            <Home />
-          </Route>
-          <Route path="/login">
-            <ProvideAuth>
-              <Login />
-            </ProvideAuth>
-          </Route>
-          <Route path="/signup">
-            <SignUpProvider>
-              <SignUp />
-            </SignUpProvider>
-          </Route>
-          <PrivateRouteLogin path="/home">
-            <UserHome />
-          </PrivateRouteLogin>
-          <PrivateRouteLogin path="/profile">
-            <UserProfile />
-          </PrivateRouteLogin>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-      </AnimatePresence>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <ProvideAuth>
+            <Login />
+          </ProvideAuth>
+        </Route>
+        <Route path="/signup">
+          <SignUpProvider>
+            <SignUp />
+          </SignUpProvider>
+        </Route>
+        <PrivateRouteLogin path="/home">
+          <UserHome />
+        </PrivateRouteLogin>
+        <PrivateRouteLogin path="/profile">
+          <UserProfile />
+        </PrivateRouteLogin>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Suspense>
   );
 }
