4108c3a94a0af9adc5ae81738e7386729627a6d9
mukul98s <mymukul112@gmail.com>
2021-08-12 23:24:21 +0530

    feat: removed global state for login

--- frontend/src/State/GlobalState.js
+++ frontend/src/State/GlobalState.js
@@ -1,22 +1,10 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext } from "react";
 
 export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [isLogin, setIsLogin] = useState(
-    JSON.parse(localStorage.getItem("isLogin"))
-  );
-
-  useEffect(() => {
-    setIsLogin(JSON.parse(localStorage.getItem("isLogin")));
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("isLogin", isLogin);
-  }, [isLogin]);
-
   return (
-    <GlobalContext.Provider value={{ isLogin, setIsLogin }}>
+    <GlobalContext.Provider value="Nothing For Now">
       {children}
     </GlobalContext.Provider>
   );
