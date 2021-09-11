9e0bb1dabc45965bd89b372e7ea91540b10f25d0
Mukul Sharma <mymukul112@gmail.com>
2021-09-11 10:45:48 +0530

    removed user Profile export

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -5,4 +5,3 @@ export const Error = React.lazy(() => import("./Error"));
 export const Login = React.lazy(() => import("./Login"));
 export const SignUp = React.lazy(() => import("./SignUp"));
 export const UserHome = React.lazy(() => import("./UserHome"));
-export const UserProfile = React.lazy(() => import("./UserProfile"));
