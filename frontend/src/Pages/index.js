2d31c1b7ecdc98433a8f8b1004675a0a99269053
Mukul Sharma <mymukul112@gmail.com>
2021-09-11 10:48:31 +0530

    remove userHome

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -4,4 +4,3 @@ export const Home = React.lazy(() => import("./Home"));
 export const Error = React.lazy(() => import("./Error"));
 export const Login = React.lazy(() => import("./Login"));
 export const SignUp = React.lazy(() => import("./SignUp"));
-export const UserHome = React.lazy(() => import("./UserHome"));
